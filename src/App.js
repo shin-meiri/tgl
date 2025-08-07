a835e9ca04ea2513ba25a586844ad842c64e8b48
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-08-07 23:06:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Kalender from './Kalender';
+import Weton from './Weton';
 
 function App() {
   return (
     <div className="App">
       <Kalender />
+    </div>
+  <div>
+      <Weton />
     </div>
   );
 }
